8fdea905c4681ac1d09511db835a56e30e3a8908
Ibtissam <benbourekibtissam@gmail.com>
2024-12-20 21:51:40 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import HeroSection from '../components/HeroSection';
+import Features from '../components/Features';
+
+const HomePage = () => {
+  return (
+    <main>
+      <HeroSection />
+      <Features />
+    </main>
+  );
+};
+
+export default HomePage;
